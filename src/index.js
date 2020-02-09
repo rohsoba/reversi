1aa3a05bf61e774b7e352920bcfa6f878eb2d108
rohsoba <rohsoba@gmail.com>
2020-02-09 23:32:35 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-let board = new Array(8).map(g => new Array(8));
-const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let board = new Array(8).fill(null).map(() => new Array(8));
+const ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function load(fumen) {
     for (let i=0; i<fumen.length; i+=3) {
         let s = fumen.substr(i, 3);
-        let x = alpha.indexOf(s[0]) + 1;
-        let y = s[1];
+        let x = ALPHA.indexOf(s[0]) + 1;
+        let y = Number(s[1]);
         let c = s[2];
         console.log(x + y + c);
         board[y][x] = c;
